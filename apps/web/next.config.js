67d1118a20c8b5b05db890a08259b6933b5cab9b
agadzik <andrew.gadzik@vercel.com>
2023-10-13 13:21:23 -0400

    fix using transpilePackages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,26 +1,10 @@
 module.exports = {
-  swcMinify: true,
-  optimizeFonts: true,
-  productionBrowserSourceMaps: false,
-  experimental: {
-    forceSwcTransforms: true,
-    esmExternals: false,
-  },
-  webpack: (config) => {
-    if (config?.module?.rules) {
-      config.module.rules = config.module.rules.map(rule => {
-        if (rule.test?.source?.includes('m?js')) {
-          return {
-            test: /\.mjs$/,
-            type: 'javascript/auto',
-            resolve: {
-              fullySpecified: false,
-            },
-          };
-        }
-        return rule;
-      });
-    }
-    return config
-  }
+  // swcMinify: true,
+  // optimizeFonts: true,
+  // productionBrowserSourceMaps: false,
+  // experimental: {
+  //   forceSwcTransforms: true,
+  //   esmExternals: false,
+  // },
+  transpilePackages: ["ui"],
 };
